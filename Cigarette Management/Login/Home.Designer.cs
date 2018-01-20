bc155bc039d6aa0408d1c7ccbddac11d46b77ea3
Chrosolva <gunawanhuang222@gmail.com>
2018-01-20 17:14:38 +0700

    First Commit

---
+++ Cigarette Management/Login/Home.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Login
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_checkdrawer = new System.Windows.Forms.Button();
+            this.btn_sendlocation = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.btn_receipt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(416, 260);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(435, 42);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "Add item";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(435, 81);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit.TabIndex = 2;
+            this.btn_edit.Text = "Edit Item";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_checkdrawer
+            // 
+            this.btn_checkdrawer.Location = new System.Drawing.Point(516, 120);
+            this.btn_checkdrawer.Name = "btn_checkdrawer";
+            this.btn_checkdrawer.Size = new System.Drawing.Size(75, 36);
+            this.btn_checkdrawer.TabIndex = 3;
+            this.btn_checkdrawer.Text = "Check Drawer";
+            this.btn_checkdrawer.UseVisualStyleBackColor = true;
+            // 
+            // btn_sendlocation
+            // 
+            this.btn_sendlocation.Location = new System.Drawing.Point(435, 120);
+            this.btn_sendlocation.Name = "btn_sendlocation";
+            this.btn_sendlocation.Size = new System.Drawing.Size(75, 36);
+            this.btn_sendlocation.TabIndex = 4;
+            this.btn_sendlocation.Text = "Send Location";
+            this.btn_sendlocation.UseVisualStyleBackColor = true;
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(516, 81);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 5;
+            this.btn_update.Text = "Update List";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(516, 42);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 6;
+            this.btn_delete.Text = "Delete Item";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Location = new System.Drawing.Point(12, 13);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(29, 13);
+            this.lbl_user.TabIndex = 7;
+            this.lbl_user.Text = "User";
+            // 
+            // btn_receipt
+            // 
+            this.btn_receipt.Location = new System.Drawing.Point(435, 173);
+            this.btn_receipt.Name = "btn_receipt";
+            this.btn_receipt.Size = new System.Drawing.Size(75, 39);
+            this.btn_receipt.TabIndex = 8;
+            this.btn_receipt.Text = "Make receipt";
+            this.btn_receipt.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 314);
+            this.Controls.Add(this.btn_receipt);
+            this.Controls.Add(this.lbl_user);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_sendlocation);
+            this.Controls.Add(this.btn_checkdrawer);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Home";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_checkdrawer;
+        private System.Windows.Forms.Button btn_sendlocation;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Button btn_receipt;
+    }
+}
