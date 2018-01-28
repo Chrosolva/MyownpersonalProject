f79d659311a0b571d8ef8457611af30f61051c6e
Chrosolva <gunawanhuang222@gmail.com>
2018-01-28 21:08:42 +0700

    ubah design

--- Cigarette Management/Login/Home.Designer.cs
+++ Cigarette Management/Login/Home.Designer.cs
@@ -37,15 +37,18 @@
             this.btn_delete = new System.Windows.Forms.Button();
             this.lbl_user = new System.Windows.Forms.Label();
             this.btn_receipt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(416, 260);
+            this.dataGridView1.Size = new System.Drawing.Size(416, 237);
             this.dataGridView1.TabIndex = 0;
             // 
             // btn_add
@@ -121,11 +124,40 @@
             this.btn_receipt.Text = "Make receipt";
             this.btn_receipt.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Drawer List";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = " Search Drawer";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(343, 37);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(86, 23);
+            this.btn_search.TabIndex = 11;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(603, 314);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_receipt);
             this.Controls.Add(this.lbl_user);
             this.Controls.Add(this.btn_delete);
@@ -136,6 +168,7 @@
             this.Controls.Add(this.btn_add);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -154,5 +187,8 @@
         private System.Windows.Forms.Button btn_delete;
         private System.Windows.Forms.Label lbl_user;
         private System.Windows.Forms.Button btn_receipt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_search;
     }
 }
